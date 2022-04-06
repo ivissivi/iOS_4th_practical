06345b6068b76ac16f8b1e09cd84fa66f1e3d4b2
Ivars Sloka <ivis31121999@gmail.com>
2022-04-06 15:20:49 +0200

    Add files via upload

---
+++ 4thPractical/4thPractical/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  4thPractical
+//
+//  Created by user215333 on 4/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 4thPractical/4thPractical/_thPracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  _thPracticalApp.swift
+//  4thPractical
+//
+//  Created by user215333 on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct _thPracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
